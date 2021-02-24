3141cf22b1d192a4deed3b8ae49a8cb911cb8887
Aldamar <danila.samusev@mail.ru>
2021-02-24 18:23:24 +0300

    Added loading icon in Team and Board pages

---
+++ JustInMindApp/JustInMindApp/ClientApp/src/components/LoadingPage.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+import '../styles/loadingPage.css'
+
+export class LoadingPage extends React.Component {
+
+    render() {
+        return (
+            <div className='loadingIcon' >
+                <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+                    <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+                </svg>
+            </div>
+        );
+    }
+}
