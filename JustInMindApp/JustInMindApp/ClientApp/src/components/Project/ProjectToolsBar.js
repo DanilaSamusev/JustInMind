6918295c191c6dda58ea2938c4c67745daf00e55
Aldamar <danila.samusev@mail.ru>
2021-06-04 14:13:22 +0300

    Test commit for CI/CD.

--- JustInMindApp/JustInMindApp/ClientApp/src/components/Project/ProjectToolsBar.js
+++ JustInMindApp/JustInMindApp/ClientApp/src/components/Project/ProjectToolsBar.js
@@ -17,9 +17,7 @@ export function ProjectToolsBar(props) {
             <div className={classes.item} >
                 <SelectProjectTool selectProject={props.selectProject} validateFetchResponse={props.validateFetchResponse} />
             </div>
-            <div className={classes.item} >
-                <DeleteProjectTool resetProject={props.resetProject} project={props.project} openSnackbar={props.openSnackbar} selectProject={props.selectProject} validateFetchResponse={props.validateFetchResponse} />
-            </div>
+            
         </div>
     );
 }
